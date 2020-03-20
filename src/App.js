5927f9a7df5ddac58de170621953ba8bf0796f08
Shivangi <shivangi.gupta0796@gmail.com>
2020-03-20 15:46:24 +0530

    add

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>My code</p>
+        <p>Shailendra Code</p>
         <a
           className="App-link"
           href="https://reactjs.org"
